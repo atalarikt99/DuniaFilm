97bd120068159617240c681828a808ed0814e540
taufan99 <atalarikt@gmail.com>
2017-06-13 11:39:09 +0700

    Added string list

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/model/Source.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm.model;
+
+import java.util.List;
+
+/**
+ * Created by Mokleters on 6/13/2017.
+ */
+
+public class Source {
+    public String id;
+    public String title;
+    public String overview;
+    public String poster_path;
+    public String category;
+    public String language;
+    public String country;
+    public List<String> sortBysAvailable;
+}
