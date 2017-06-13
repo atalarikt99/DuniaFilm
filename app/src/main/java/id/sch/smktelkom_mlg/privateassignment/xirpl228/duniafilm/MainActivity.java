1202a0ec2d764b3f370c2df5ec29c0dc5b373e7c
taufan99 <atalarikt@gmail.com>
2017-06-13 10:54:39 +0700

    Added Toolbar

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/MainActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     }
 }
