0e876301ee86285b8a45baeaf08643abf49b9593
taufan99 <atalarikt@gmail.com>
2017-06-13 10:46:29 +0700

    Initial commit

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/adapter/SourceAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/adapter/SourceAdapter.java
@@ -1,8 +1,11 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm.adapter;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by Mokleters on 6/13/2017.
  */
 
-public class SourceAdapter {
+public class SourceAdapter extends RecyclerView.Adapter<SourceAdapter.ViewHolder> {
+    public static final String IMAGE_URL_BASE_PATH = "http://image.tmdb.org/t/p/w500";
 }
