b60948421c195f1ae6b86706d39f3609ff8ff9ff
taufan99 <atalarikt@gmail.com>
2017-06-13 10:52:48 +0700

    Added list and context

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/adapter/SourceAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/adapter/SourceAdapter.java
@@ -1,6 +1,11 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import javax.xml.transform.Source;
 
 /**
  * Created by Mokleters on 6/13/2017.
@@ -8,4 +13,12 @@ import android.support.v7.widget.RecyclerView;
 
 public class SourceAdapter extends RecyclerView.Adapter<SourceAdapter.ViewHolder> {
     public static final String IMAGE_URL_BASE_PATH = "http://image.tmdb.org/t/p/w500";
+    ArrayList<Source> list;
+    Context context;
+
+    public SourceAdapter(Context context, ArrayList<Source> list)
+    {
+        this.list = list;
+        this.context = context;
+    }
 }
