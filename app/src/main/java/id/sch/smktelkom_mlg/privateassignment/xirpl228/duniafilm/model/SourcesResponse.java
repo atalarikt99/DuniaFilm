3f074880b78018ed885e80ceb1857a87fbe339f6
taufan99 <atalarikt@gmail.com>
2017-06-13 11:37:01 +0700

    SourcesResponse completed

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/model/SourcesResponse.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/model/SourcesResponse.java
@@ -1,8 +1,14 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm.model;
 
+import java.util.List;
+
+import javax.xml.transform.Source;
+
 /**
  * Created by Mokleters on 6/13/2017.
  */
 
 public class SourcesResponse {
+    public String page;
+    public List<Source> results;
 }
