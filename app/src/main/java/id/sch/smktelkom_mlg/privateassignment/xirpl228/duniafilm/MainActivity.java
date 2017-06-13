8392ecfb63ea2849e088d4b128987160ffad175c
taufan99 <atalarikt@gmail.com>
2017-06-13 10:29:39 +0700

    Added SectionsPagerAdapter and ViewPager

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/duniafilm/MainActivity.java
@@ -1,9 +1,14 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl228.duniafilm;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
